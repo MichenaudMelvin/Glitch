f52de27eab380b40abb22c701b8ad3581162984b
Melvin <74654895+MichenaudMelvin@users.noreply.github.com>
2022-10-26 10:00:03 +0200

    Initial commit

---
+++ GlitchUE/Source/GlitchUE/GlitchUE.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GlitchUE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GlitchUE, "GlitchUE" );
+ 
---
+++ GlitchUE/Source/GlitchUE/GlitchUEGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GlitchUEGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGlitchUEGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGlitchUEGameMode();
+};
+
+
+
