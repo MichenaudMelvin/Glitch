db249a3bba0db7ed9ad86580fbe341392a835168
Melvin <74654895+MichenaudMelvin@users.noreply.github.com>
2023-01-07 18:03:19 +0100

    ADD: Delete Save tool

---
+++ GlitchUE/Source/GlitchUE/Public/Saves/AbstractSave.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "AbstractSave.generated.h"
+
+UCLASS()
+class GLITCHUE_API UAbstractSave : public USaveGame{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FString SlotName;
+};
