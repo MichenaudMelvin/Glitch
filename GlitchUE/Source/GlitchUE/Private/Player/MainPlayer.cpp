609455c05f923562935245cfac37df1607598403
Melvin <74654895+MichenaudMelvin@users.noreply.github.com>
2023-01-26 14:12:49 +0100

    UPDATE: ai now destroy when the tower defense phase start and others fixes

--- GlitchUE/Source/GlitchUE/Private/Player/MainPlayer.cpp
+++ GlitchUE/Source/GlitchUE/Private/Player/MainPlayer.cpp
@@ -59,6 +59,8 @@ AMainPlayer::AMainPlayer(){
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
 
+	GetMesh()->SetReceivesDecals(false);
+
 	#pragma region Timelines
 
 	static ConstructorHelpers::FObjectFinder<UCurveFloat> Curve(TEXT("/Game/Blueprint/Curves/FC_ZeroToOneCurve"));
