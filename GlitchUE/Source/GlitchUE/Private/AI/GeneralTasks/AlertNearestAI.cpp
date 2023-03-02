8a196a72cff50ba0700866aeb520c3fe9173a08d
Melvin <74654895+MichenaudMelvin@users.noreply.github.com>
2023-03-02 11:05:17 +0100

    UPDATE: Drone Pursuit now follow when the level is alerted

--- GlitchUE/Source/GlitchUE/Private/AI/GeneralTasks/AlertNearestAI.cpp
+++ GlitchUE/Source/GlitchUE/Private/AI/GeneralTasks/AlertNearestAI.cpp
@@ -3,43 +3,27 @@
 
 #include "AI/GeneralTasks/AlertNearestAI.h"
 #include "AI/MainAIController.h"
+#include "AI/AIPursuitDrone/PursuitDrone.h"
+#include "AI/AIPursuitDrone/PursuitDroneController.h"
 #include "BehaviorTree/BlackboardComponent.h"
-#include "BehaviorTree/Blackboard/BlackboardKeyType_Object.h"
 #include "Helpers/FunctionsLibrary/UsefullFunctions.h"
 #include "Kismet/GameplayStatics.h"
 
-UAlertNearestAI::UAlertNearestAI(){
-	Player.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UAlertNearestAI, Player), AActor::StaticClass());
-}
-
-void UAlertNearestAI::InitializeFromAsset(UBehaviorTree& Asset){
-	Super::InitializeFromAsset(Asset);
-
-	const UBlackboardData* BBAsset = GetBlackboardAsset();
-	if (ensure(BBAsset)){
-		Player.ResolveSelectedKey(*BBAsset);
-	}
-}
-
 EBTNodeResult::Type UAlertNearestAI::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory){
-	const UBlackboardComponent* CurrentBlackboard = OwnerComp.GetBlackboardComponent();
 	AIList.Empty();
 
 	TArray<AActor*> MainAIControllerArray;
-	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AMainAIController::StaticClass(), MainAIControllerArray);
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), APursuitDroneController::StaticClass(), MainAIControllerArray);
 
 	for(int i = 0; i < MainAIControllerArray.Num(); i++){
-		AIList.Add(Cast<AMainAIController>(MainAIControllerArray[i])->GetPawn());
+		AIList.Add(Cast<APursuitDroneController>(MainAIControllerArray[i])->GetPawn());
 	}
 
 	UUsefullFunctions::SortActorsByDistanceToActor(AIList, OwnerComp.GetAIOwner()->GetPawn());
 
-	const FVector PlayerLocation = Cast<AActor>(CurrentBlackboard->GetValue<UBlackboardKeyType_Object>(Player.GetSelectedKeyID()))->GetActorLocation();
-
 	for(int i = 0; i < NumberOfAIToCall; i++){
 		if(IsValid(AIList[i])){
-			UBlackboardComponent* CurrentBlackboardComp = Cast<AMainAIController>(AIList[i])->GetBlackboardComponent();
-			CurrentBlackboardComp->SetValueAsVector("AlertedLocation", PlayerLocation);
+			UBlackboardComponent* CurrentBlackboardComp = Cast<APursuitDrone>(AIList[i])->GetBlackBoard();
 			CurrentBlackboardComp->SetValueAsBool("ReceiveAlert", true);
 		}
 	}
