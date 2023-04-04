60af915c6d055ab741429672225446dbc0b92b58
Melvin <74654895+MichenaudMelvin@users.noreply.github.com>
2023-04-04 10:43:50 +0200

    ADD: Focus dorones anim + update their behaviors

--- GlitchUE/Source/GlitchUE/Private/AI/GeneralTasks/AttackTarget.cpp
+++ GlitchUE/Source/GlitchUE/Private/AI/GeneralTasks/AttackTarget.cpp
@@ -4,11 +4,14 @@
 #include "AI/GeneralTasks/AttackTarget.h"
 #include "AI/MainAIController.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "BehaviorTree/Blackboard/BlackboardKeyType_Bool.h"
 #include "BehaviorTree/Blackboard/BlackboardKeyType_Object.h"
 #include "Components/HealthComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
 UAttackTarget::UAttackTarget(){
 	TargetToAttack.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UAttackTarget, TargetToAttack), AActor::StaticClass());
+	AttackKey.AddBoolFilter(this, GET_MEMBER_NAME_CHECKED(UAttackTarget, AttackKey));
 }
 
 void UAttackTarget::InitializeFromAsset(UBehaviorTree& Asset){
@@ -21,7 +24,9 @@ void UAttackTarget::InitializeFromAsset(UBehaviorTree& Asset){
 }
 
 EBTNodeResult::Type UAttackTarget::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory){
-	const UBlackboardComponent* CurrentBlackboard = OwnerComp.GetBlackboardComponent();
+	UBlackboardComponent* CurrentBlackboard = OwnerComp.GetBlackboardComponent();
+
+	APawn* AIPawn = OwnerComp.GetAIOwner()->GetPawn();
 
 	const AActor* Target = Cast<AActor>(CurrentBlackboard->GetValue<UBlackboardKeyType_Object>(TargetToAttack.GetSelectedKeyID()));
 
@@ -29,6 +34,11 @@ EBTNodeResult::Type UAttackTarget::ExecuteTask(UBehaviorTreeComponent& OwnerComp
 		UHealthComponent* TargetHealthComp = Cast<UHealthComponent>(Target->GetComponentByClass(UHealthComponent::StaticClass()));
 
 		if(TargetHealthComp->GetCanTakeDamages()){
+			CurrentBlackboard->SetValue<UBlackboardKeyType_Bool>(AttackKey.SelectedKeyName, true);
+			const FRotator TargetRotation = UKismetMathLibrary::FindLookAtRotation(AIPawn->GetActorLocation(), Target->GetActorLocation());
+
+			AIPawn->SetActorRotation(TargetRotation);
+
 			TargetHealthComp->TakeDamages(Cast<AMainAIController>(OwnerComp.GetAIOwner())->GetDamages());
 			return EBTNodeResult::Succeeded;
 		}
